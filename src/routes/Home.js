b28ca49d91463854fc936973406e639093324046
AmandaFrisk <amanda.frisk33@gmail.com>
2023-05-05 11:46:04 -0600

    comment out testimonial component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,14 +3,14 @@ import React from 'react'
 // import Navbar1 from '../components/Navbar1'
 
 // import Hero from '../components/Hero'
-import Testimonial from '../components/Testimonial'
+// import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
     <>
     {/* <Navbar1/> */}
     {/* <Hero/> */}
-     <Testimonial/>
+     {/* <Testimonial/> */}
   </>
   )
 }
