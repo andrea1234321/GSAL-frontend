81ee429402ae4a1401091076c3c427febf784f9c
Andrea <andrearewards@hotmail.com>
2023-07-05 11:34:28 -0400

    add endpoint to contact form for submission

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,10 +9,10 @@ const ContactForm = () => {
           Contact
           </h1>
         <p className='contact-info'>Thank you for your interest in GoSaveALife, a non-profit organization dedicated to raising awareness about kidney donation and making a positive impact on the lives of those in need. Whether you're looking to learn more about kidney donation or have other inquiries related to our cause, we are here to assist you. Please fill out the contact form below, and we will respond to your message as soon as possible.</p>
-        <form className='contact-form'>
-          <input className='contact-name' placeholder='Name'></input>
-          <input className='contact-email' placeholder='Email'></input>
-          <textarea className='contact-message' placeholder='Message' rows='4' cols='50' ></textarea>
+        <form className='contact-form' action='https://getform.io/f/e8adcf4e-245c-4dbd-ad88-c608836c8932' method="POST">
+          <input className='contact-name' placeholder='Name' type='text' name='name'></input>
+          <input className='contact-email' placeholder='Email' type='email' name='email'></input>
+          <textarea className='contact-message' placeholder='Message' rows='4' cols='50' name='textarea'></textarea>
           <button className='contact-submit' type='submit'>Send Message</button>
         </form>
       </div>
